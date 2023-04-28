5e161a5dbf2dd6017ef392c22eb53eaf22d171d7
NikitaaaGudkov <nikita.gudkov.2015@mail.ru>
2023-04-29 00:23:58 +0300

    add_task_2_in_DZ_3

---
+++ DZ_3/task_2.java
@@ -0,0 +1,33 @@
+package DZ_3;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+// Пусть дан произвольный список целых чисел, удалить из него чётные числа
+
+public class task_2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = FillArrayList();
+        System.out.println(numbers);
+
+        numbers = DeleteEvenNumbers(numbers);
+        System.out.println(numbers);
+    }
+
+    public static ArrayList<Integer> FillArrayList() {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < new Random().nextInt(5, 10); ++i) {
+            result.add(new Random().nextInt(1, 100));
+        }
+        return result;
+    }
+
+    public static ArrayList<Integer> DeleteEvenNumbers(ArrayList<Integer> result) {
+        for (int i = result.size() - 1; i >= 0; --i) {
+            if(result.get(i) % 2 == 0) {
+                result.remove(i);
+            }
+        }
+        return result;
+    }
+}
